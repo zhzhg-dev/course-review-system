b086dae5fd86b131f9170aa724daab1e33b09789
zhzhg-dev <zhzhg95@gmail.com>
2026-08-06 22:25:24 +1200

    Create profile.js

---
+++ frontend/src/utils/profile.js
@@ -0,0 +1,12 @@
+export function getInitials(name) {
+  if (name === undefined)
+    return ""
+  return name
+    .split(" ")
+    .filter(Boolean)
+    .map((part) => part[0])
+    .join("")
+    .slice(0, 2)
+    .toUpperCase();
+}
+
